9ce54b129c3ab542b6b40e8f124bd63e79e68b01
Belli <ivanlorenzana@outlok.com>
2024-08-30 23:46:30 -0700

    GymFit_5: Removed unused code

---
+++ GymFit/Views/Sessions/SessionsView.swift
@@ -0,0 +1,18 @@
+//
+//  SessionsView.swift
+//  GymFit
+//
+//  Created by Ivan Lorenzana Belli on 30/08/24.
+//
+
+import SwiftUI
+
+struct SessionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SessionsView()
+}
