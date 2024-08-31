75c8cf13197357933b713832ab5275e5216732e3
Belli <ivanlorenzana@outlok.com>
2024-08-30 22:24:06 -0700

    GymFit_3: Added UIConstants and AppStrings

---
+++ GymFit/Resources/AppStrings.swift
@@ -0,0 +1,17 @@
+//
+//  AppStrings.swift
+//  GymFit
+//
+//  Created by Ivan Lorenzana Belli on 30/08/24.
+//
+
+import Foundation
+
+struct AppStrings {
+    struct Home {
+        static let announcementsTitle = NSLocalizedString(
+            "announcements_Title",
+            value:"Announcements",
+            comment: "Announcements Title")
+    }
+}
